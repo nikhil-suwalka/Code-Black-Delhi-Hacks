8727ba9d35b8d85c7c632a4a8e9e176ba0b7aac6
Nishit Shanbhag <nishit.shanbhag@gmail.com>
2020-09-05 18:03:45 +0530

    Fixed html pages

--- quizzle/main_app/views.py
+++ quizzle/main_app/views.py
@@ -1,17 +1,41 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
+
+
 # Create your views here.
 
 
 def home_view(request):
+    print("qwerty", request.session)
     if request.session.get("session_id", False):
-        s = Session.objects.create()
-        request.session["session_id"] = s.session_id
-        print(request.session.get("Session created: " + str(s)))
+        print("Old session exists: ", request.session.get("session_id"))
 
-        return HttpResponse("You've newwwww: " +str(s))
+        return render(request,"home.html",{})
+
+
     else:
-        print(request.session.get("session_id"))
-        return render(request,'home.html',{})
+        s = Session.objects.create()
+        request.session["session_id"] = s.id
+        print("New session created: ", s.id)
 
+        return render(request,"home.html",{})
+
+
+def deleteSession(request):
+    try:
+        del request.session["session_id"]
+        request.session.modified = True
+        return True
+    except:
+        return False
+
+
+def createQuestion(que: str, session_id: int) -> int:
+    q = Question.objects.create(session_id=Session.objects.filter(id=session_id).first(), question=que)
+    return q.id
+
+
+def createOption(option_text: str, question_id: int, session_id: int) -> None:
+    o = Option.objects.create(session_id=Session.objects.filter(id=session_id).first(),
+                              question_id=Question.objects.filter(id=question_id).first(), option=option_text)

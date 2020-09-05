85f57991b716d2d3f5b2fb14482ba36dea8bd01e
Nikhil Suwalka <suwalka18@gmail.com>
2020-09-05 16:04:05 +0530

    Created DB

--- quizzle/main_app/models.py
+++ quizzle/main_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Session(models.Model):
+    session_id = models.IntegerField(blank=False, null=False, unique=True, primary_key=True)
+    create_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.session_id
+
+
+class Question(models.Model):
+    question_id = models.IntegerField(blank=False, null=False, unique=True, primary_key=True)
+    session_id = models.ForeignKey(Session, on_delete=models.CASCADE, related_name="session_link")
+    question = models.CharField(max_length=255, blank=False, null=False)
+
+    def __str__(self):
+        return self.question
+
+
+class Option(models.Model):
+    option_id = models.IntegerField(blank=False, null=False, unique=True, primary_key=True)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="question_link")
+
+    def __str__(self):
+        return self.option_id
